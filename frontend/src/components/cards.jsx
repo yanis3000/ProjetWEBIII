e33f4ddd0f051c0cb43cf7b7c4b7054b690df82f
Yanis <yanimosoruz@outlook.com>
2025-11-19 00:32:33 -0500

    feat : card complete ?

--- frontend/src/components/cards.jsx
+++ frontend/src/components/cards.jsx
@@ -1,22 +1,22 @@
 import '../css/global.css'
 
-export default function MyCard({texte = "", description=""}) {
+export default function MyCard({texte = "", description="", hp="", atk="", cost=""}) {
   return (
     <div className='container'>
       <div className='badge'>
-        <p>67</p>
+        <p>{hp}</p>
       </div>
       <div className='card'>
         <img 
-          src={"https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"} 
+          src={"https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"} // assigner une image apres 
           alt={"Pas d'image assignée"} 
           style={{ width: '200px', height: '200px'}} />
         <p className='name'>{texte}</p>
-        <p>{description}</p>
+        <p className="description">{description}</p>
 
         <div className='state'>
-          <p className='attack'>76</p>
-          <p className='mana'>67</p>
+          <p className='attack'>{atk}</p>
+          <p className='cost'>{cost}</p>
         </div>
 
         <p></p>
